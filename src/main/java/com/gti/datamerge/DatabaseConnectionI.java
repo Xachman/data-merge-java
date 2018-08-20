d6d01d599b229639f276b65e66fc7656a7f53f32
xach <zironside@hotmail.com>
2018-08-20 07:56:48 -0400

    add DatabaseConnection Interface

---
+++ src/main/java/com/gti/datamerge/DatabaseConnectionI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gti.datamerge;
+
+import com.gti.datamerge.database.Row;
+import com.gti.datamerge.database.Table;
+import java.util.List;
+
+/**
+ *
+ * @author Xachman
+ */
+public interface DatabaseConnectionI {
+	public Row get(Table table, int id);	
+	public List<Row> getAll(Table table);	
+	public List<Row> getAll(String tableName);	
+	public List<Table> getAllTables();	
+}
