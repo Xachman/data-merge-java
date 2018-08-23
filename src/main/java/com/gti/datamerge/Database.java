de6a39caba662087aa92109e545e9b229c98fc17
xach <zironside@hotmail.com>
2018-08-23 10:23:49 -0400

    work on relationship table merges

--- src/main/java/com/gti/datamerge/Database.java
+++ src/main/java/com/gti/datamerge/Database.java
@@ -19,6 +19,7 @@ import java.util.Set;
 public class Database {
 	private List<Table> tables;
 	private DatabaseConnectionI dbc;
+    private Database mergeDB;
     private List<Action> actions = new ArrayList<>();
 
 	public Database(DatabaseConnectionI dbc) {
@@ -44,40 +45,44 @@ public class Database {
 	private Action insertAction(Row row, Table table) {
 		return new Action(Action.INSERT, row, table.getName());
 	}	
-    private void actionsByRelation(Table table, String id, Database db, int relationId) {
-		List<Row> dbRows = db.getRows(table.getName());
-        Relationship relationship = table.getRelationship();
-        int increment = table.getIncrement();
-        for(Row row: dbRows) {
-
-            if(row.getVal(relationship.getColumn()).equals(id)) {
-                increment++;
-                row.put(relationship.getColumn(), Integer.toString(relationId));
-                addAction(Action.INSERT, row, table, increment);
+    private List<Action> getActionsByTable(String tableName) {
+        List<Action> result = new ArrayList<>();
+        for(Action action: actions) {
+            if(action.getTableName().equals(tableName)) {
+                result.add(action);
             }
         }
-        
+        return result;
     }
     public List<Action> mergeTableActions(String tableName, Database db) throws Exception {
+        mergeDB = db;
 		List<Row> rows = db.getRows(tableName);
 		List<Row> dbRows = getRows(tableName);
         Table table = getTable(tableName);
         List<Table> relatedTables = dbc.getRelatedTables(table);
+        Relationship relationship = table.getRelationship();
         int increment = table.getIncrement();
 
         System.out.println(relatedTables);
 
-		for(Row row: rows) {
-			if(!isRowInRows(row, dbRows)){
-                String id = row.getVal(table.getPrimaryKey());
-                increment++;
-                addAction(Action.INSERT, row, table, increment);
-                for(Table rTable: relatedTables) {
-                    actionsByRelation(rTable, id, db, increment);
+        if(relationship == null) {
+            for(Row row: rows) {
+                if(!isRowInRows(row, dbRows)){
+                    increment++;
+                    addAction(Action.INSERT, row, table, increment);
                 }
-			}
-		}
-	
+            }
+		} else {
+            List<Action> rActions = getActionsByTable(relationship.getTable());
+            addActionsForTable(table, rActions, db);  
+        }
+        
+
+        if(relatedTables.size() > 0) {
+            for(Table rTable: relatedTables) {
+                mergeTableActions(rTable.getName(), db);
+            }
+        }
 		return actions;	
     }
 
@@ -108,5 +113,42 @@ public class Database {
         }
         return false;
     } 
+
+    private void addActionsForTable(Table table, List<Action> rActions, Database db) {
+        // TODO: need old id
+        int increment = table.getIncrement();
+        for(Action action: rActions){
+            String column = table.getRelationship().getColumn();
+            List<Row> rows = getRowsByColumnVal(column, action.getData().getVal(column), db.getRows(table.getName()));         
+            for(Row row: rows) {
+                row.put(column, Integer.toString(increment));
+                addAction(Action.INSERT, row, table, increment);
+            }
+        }
+    }
+    private void actionsByRelation(Table table, String id, Database db, int relationId) {
+		List<Row> dbRows = db.getRows(table.getName());
+        Relationship relationship = table.getRelationship();
+        int increment = table.getIncrement();
+        for(Row row: dbRows) {
+
+            if(row.getVal(relationship.getColumn()).equals(id)) {
+                increment++;
+                row.put(relationship.getColumn(), Integer.toString(relationId));
+                addAction(Action.INSERT, row, table, increment);
+            }
+        }
+        
+    }
+
+    private List<Row> getRowsByColumnVal(String column, String val, List<Row> rows) {
+        List<Row> result = new ArrayList<>();
+        for(Row row: rows) {
+            if(row.getVal(column).equals(val)) {
+                result.add(row);
+            }
+        }
+        return result;
+    }
 }
 
